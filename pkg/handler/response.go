64eb58d888c5f9e9b8b82a24dd342182cfbb48fd
motya <motyazz32@gmail.com>
2024-11-26 13:43:00 +0300

    changed struct to map in abortWithStatusJSON for code style consistency reasons

--- pkg/handler/response.go
+++ pkg/handler/response.go
@@ -6,13 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type responseError struct {
-	Message string `json:"message"`
-}
-
 func abortWithStatusCode(ctx *gin.Context, statusCode int, msg string) {
 	slog.Error(msg)
-	// alternative is to use map[string]string but ig it takes more time and space than simple struct
-	// I might be wrong abt that one 0_()
-	ctx.AbortWithStatusJSON(statusCode, responseError{msg})
+	ctx.AbortWithStatusJSON(statusCode, map[string]string{"message": msg})
 }
